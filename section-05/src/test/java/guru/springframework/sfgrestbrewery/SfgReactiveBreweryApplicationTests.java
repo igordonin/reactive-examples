077a5cc8b606175dd9dcb9c1f06beb40b755dea5
igordonin <igordonin@gmail.com>
2022-07-23 14:24:28 -0300

    changing dependencies to use webflux instead of web starter

--- section-05/src/test/java/guru/springframework/sfgrestbrewery/SfgReactiveBreweryApplicationTests.java
+++ section-05/src/test/java/guru/springframework/sfgrestbrewery/SfgReactiveBreweryApplicationTests.java
@@ -1,14 +1,11 @@
 package guru.springframework.sfgrestbrewery;
 
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class SfgReactiveBreweryApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
-
+  @Test
+  public void contextLoads() {}
 }
