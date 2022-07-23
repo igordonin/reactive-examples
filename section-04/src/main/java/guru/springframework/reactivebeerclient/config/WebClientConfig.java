635726d5dc68a4de818dc0a623cd3340dce8a932
igordonin <igordonin@gmail.com>
2022-07-23 10:46:05 -0300

    adding some logging configs for wire tapping

--- section-04/src/main/java/guru/springframework/reactivebeerclient/config/WebClientConfig.java
+++ section-04/src/main/java/guru/springframework/reactivebeerclient/config/WebClientConfig.java
@@ -2,13 +2,26 @@ package guru.springframework.reactivebeerclient.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import static io.netty.handler.logging.LogLevel.DEBUG;
+import static reactor.netty.transport.logging.AdvancedByteBufFormat.TEXTUAL;
 
 @Configuration
 public class WebClientConfig {
 
   @Bean
   public WebClient webClient() {
-    return WebClient.builder().baseUrl(WebClientProperties.BASE_URL).build();
+    return WebClient.builder()
+        .baseUrl(WebClientProperties.BASE_URL)
+        .clientConnector(this.createConnector())
+        .build();
+  }
+
+  private ReactorClientHttpConnector createConnector() {
+    final String clientClass = "reactor.netty.client.HttpClient";
+    return new ReactorClientHttpConnector(HttpClient.create().wiretap(clientClass, DEBUG, TEXTUAL));
   }
 }
